0e82dfc407c3c1dfdbbfe8a1a727ef11210528c0
Petya Ivanchova <pepivanchova@yahoo.com>
2024-01-04 19:04:33 +0200

    add react app and homePage, shopPage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import BannerComponent from './components/BannerComponent';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import HomePage from './pages/HomePage';
+import ShopPage from './pages/ShopPage';
+
+function App() {
+  return (
+    <>
+      <ShopPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import HeaderComponent from "../components/HeaderComponent";
+import BannerComponent from "../components/BannerComponent";
+import FooterComponent from "../components/FooterComponent";
+
+const HomePage = () => {
+    return (
+        <>
+            <HeaderComponent />
+            <BannerComponent />
+            <FooterComponent />
+        </>
+    );
+};
+
+export default HomePage;
